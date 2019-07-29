c654c6cdfc316434cd00b5f6f6850dbc1570eb7e
Nathan Michlo <1386161@students.wits.ac.za>
2019-07-29 20:46:04 +0200

    LICENSE

--- kolr/kolred.py
+++ kolr/kolred.py
@@ -1,7 +1,31 @@
 
+#  MIT License
+#
+#  Copyright (c) 2019 Nathan Juraj Michlo
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+
 # ========================================================================= #
-# Kolrs                                                                     #
+# KOLRS                                                                     #
 # ========================================================================= #
+
 
 # TODO: replace with more robust methods in kolr.term.sgr_params
 # TODO: remove duplicated effort and use kolr.term.sgr_params instead
@@ -111,3 +135,6 @@ if __name__ == '__main__':
     print(Kolr('left ' + ' right' + 'fdsa', color='lgreen'))
     print(str(Kolr('left ' + Kolr('inner', color='lred')('asdf', color='reset') + ' right', color='lgreen')))
 
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
